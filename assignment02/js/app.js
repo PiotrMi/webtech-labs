75cf5f1a6c3935e651b43d8bad22831f06eb1071
Oleksandr Sabov <alexsabov@gmail.com>
2014-10-23 10:51:03 +0200

    Add mapbox

--- assignment02/js/app.js
+++ assignment02/js/app.js
@@ -1,2 +1,6 @@
-
-console.log("Hello world");
+$(function() {
+    $map = $('.map');
+    console.log(L);
+    L.mapbox.accessToken = 'pk.eyJ1Ijoic2Fib3YiLCJhIjoiN2ZfYTFNZyJ9.NMb6Hc8OwHRiHDPMjbzZGg';
+    var layer = L.mapbox.map($map.get(0), 'examples.map-zr0njcqy');
+});
