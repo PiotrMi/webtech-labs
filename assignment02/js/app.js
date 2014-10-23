d3eb58fd1c67fc5b0be8e956f5fcb6906070cd9c
Oleksandr Sabov <alexsabov@gmail.com>
2014-10-23 11:13:45 +0200

    Add geolocation call

--- assignment02/js/app.js
+++ assignment02/js/app.js
@@ -1,6 +1,36 @@
 $(function() {
-    $map = $('.map');
-    console.log(L);
+    var $map = $('.map');
+    var $body = $('body');
     L.mapbox.accessToken = 'pk.eyJ1Ijoic2Fib3YiLCJhIjoiN2ZfYTFNZyJ9.NMb6Hc8OwHRiHDPMjbzZGg';
-    var layer = L.mapbox.map($map.get(0), 'examples.map-zr0njcqy');
+
+    var map = L.mapbox.map($map.get(0), 'examples.map-zr0njcqy');
+    map.setView([38.91338, -77.03236], 16);
+
+    showPosition = function(position) {
+        console.log(position);
+        pos = [position.coords.latitude, position.coords.longitude];
+        map.panTo(pos);
+        //L.marker(pos).addTo(map);
+        L.mapbox.featureLayer({
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: pos.reverse()
+            },
+            properties: {
+                title: 'Peregrine Espresso',
+                description: '1718 14th St NW, Washington, DC',
+                'marker-size': 'large',
+                'marker-color': '#3445E2',
+                'marker-symbol': 'star'
+            }
+        }).addTo(map);
+    }
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        $body.html("Geolocation is not supported by this browser.");
+    }
+
 });
