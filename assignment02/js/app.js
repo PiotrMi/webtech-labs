4b33ca35fca7b7ecb688f4cb76c8d089f8b2b793
Oleksandr Sabov <alexsabov@gmail.com>
2014-11-02 20:44:55 +0100

    Add base layout

--- assignment02/js/app.js
+++ assignment02/js/app.js
@@ -1,10 +1,9 @@
 
-var cinema = [
+var cinemas = [
     [10,20],
     [11,21],
     [14,24],
-    [17,25],
-    [12,25]
+    [17,25]
 ];
 
 function crateMarket(map, pos) {
@@ -28,41 +27,55 @@ function crateMarket(map, pos) {
 
 $(function() {
     var $map = $('.map');
-    var $body = $('body');
-    L.mapbox.accessToken = 'pk.eyJ1Ijoic2Fib3YiLCJhIjoiN2ZfYTFNZyJ9.NMb6Hc8OwHRiHDPMjbzZGg';
+    if($map.length) {
 
-    var map = L.mapbox.map($map.get(0), 'sabov.k1c8mj31');
-    map.setZoom(14);
+        $body = $('body');
+        L.mapbox.accessToken = 'pk.eyJ1Ijoic2Fib3YiLCJhIjoiN2ZfYTFNZyJ9.NMb6Hc8OwHRiHDPMjbzZGg';
 
-    showPosition = function(position) {
-        pos = [position.coords.latitude, position.coords.longitude];
-        map.panTo(pos);
-        //L.marker(pos).addTo(map);
-        L.mapbox.featureLayer({
-            type: 'Feature',
-            geometry: {
-                type: 'Point',
-                coordinates: pos.reverse()
-            },
-            properties: {
-                title: 'Peregrine Espresso',
-                description: '1718 14th St NW, Washington, DC',
-                'marker-size': 'large',
-                'marker-color': '#3445E2',
-                'marker-symbol': 'star'
-            }
-        }).addTo(map);
-        cinema.forEach(function() {
-            newPos = [pos[1] + _.random(-10,10)/1000, pos[0] + _.random(-10,10)/1000];
-            crateMarket(map, newPos);
-        });
-        $('.loading').animate({opacity: 0}, 1000);
+        var map = L.mapbox.map($map.get(0), 'sabov.k1c8mj31');
+        map.setZoom(14);
+
+        showPosition = function(position) {
+            pos = [position.coords.latitude, position.coords.longitude];
+            map.panTo(pos);
+            //L.marker(pos).addTo(map);
+            L.mapbox.featureLayer({
+                type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                    coordinates: pos.reverse()
+                },
+                properties: {
+                    title: 'Peregrine Espresso',
+                    description: '1718 14th St NW, Washington, DC',
+                    'marker-size': 'large',
+                    'marker-color': '#3445E2',
+                    'marker-symbol': 'star'
+                }
+            }).addTo(map);
+            cinemas.forEach(function() {
+                newPos = [pos[1] + _.random(-10,10)/1000, pos[0] + _.random(-10,10)/1000];
+                crateMarket(map, newPos);
+            });
+            $('.loading').animate({opacity: 0}, 1000);
+        }
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            $body.html("Geolocation is not supported by this browser.");
+        }
     }
+});
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        $body.html("Geolocation is not supported by this browser.");
-    }
 
+$(function() {
+    $(".list__link").on('click', function(e) {
+        var target = this.href.split('#')[1];
+        $('.list__item').removeClass('active');
+        $(this).closest('.list__item').addClass('active');
+        $('section').animate({opacity: 0}).hide();
+        $('#'+target).show().animate({opacity: 1});
+        return false;
+    });
 });
